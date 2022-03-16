a52fb6695011118dd67ad2f954e30e472430acf8
Italo Bartolomeu <italobartolomeugarcia@gmail.com>
2022-03-16 16:12:38 -0300

    inicio projeto SimpleForm

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField  from "@material-ui/core/TextField";
+
+function FormularioCadastro() {
+  return (
+    <form>
+      <TextField id="nome" label="Nome" variant="outlined" fullWidth/>
+      <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth/>
+      <TextField id="CPF" label="CPF" variant="outlined" fullWidth/>
+
+
+      <label>Promoções</label>
+      <input type="checkbox" />
+      <label>Novidades</label>
+      <input type="checkbox" />
+
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
+    </form>
+  );
+}
+
+export default FormularioCadastro;

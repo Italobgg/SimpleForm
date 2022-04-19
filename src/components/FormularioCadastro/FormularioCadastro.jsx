2529bb966d1e199826e7e22ec5b17315e86129dc
Italo Bartolomeu <italobartolomeugarcia@gmail.com>
2022-04-19 14:36:25 -0300

    controlando ciclo de vida, contextos e providers, hooks customizados

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,43 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
-import { StepLabel, Stepper, Typography, Step } from "@material-ui/core";
-import { useEffect } from "react";
+import { Typography, Stepper, Step, StepLabel } from "@material-ui/core";
 
-function FormularioCadastro({ aoEnviar, validarCPF }) {
+function FormularioCadastro({ aoEnviar }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
   const [dadosColetados, setDados] = useState({});
 
-  useEffect(()=>{
-    if(etapaAtual === formularios.length-1){
+  useEffect(() => {
+    if (etapaAtual === formularios.length - 1) {
       aoEnviar(dadosColetados);
     }
-  })
-
-
+  });
 
   const formularios = [
     <DadosUsuario aoEnviar={coletarDados} />,
-    <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
+    <DadosPessoais aoEnviar={coletarDados} />,
     <DadosEntrega aoEnviar={coletarDados} />,
-    <Typography variant="h5">Obrigado por se cadastrar!</Typography>
+    <Typography variant="h5">Obrigado pelo Cadastro!</Typography>,
   ];
 
-  function coletarDados(dados){
-    setDados({...dadosColetados, ...dados});
+  function coletarDados(dados) {
+    setDados({ ...dadosColetados, ...dados });
     proximo();
   }
   function proximo() {
     setEtapaAtual(etapaAtual + 1);
   }
-
-  return ( <>
+  return (
+    <>
       <Stepper activeStep={etapaAtual}>
-        <Step><StepLabel>Login</StepLabel></Step>
-        <Step><StepLabel>Pessoal</StepLabel></Step>
-        <Step><StepLabel>Entrega</StepLabel></Step>
-        <Step><StepLabel>Finalização</StepLabel></Step>
+        <Step>
+          <StepLabel>Login</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Pessoal</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Entrega</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Finalização</StepLabel>
+        </Step>
       </Stepper>
       {formularios[etapaAtual]}
     </>

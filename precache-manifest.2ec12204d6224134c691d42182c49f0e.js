aff9dae961f80a1a884a8c5dd97790000157b1fe
italo <italobartolomeugarcia@gmail.com>
2022-11-13 15:31:25 -0300

    Updates

---
+++ precache-manifest.2ec12204d6224134c691d42182c49f0e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "86898c4d62787336f862",
+    "url": "%7Busername%7D.github.io/%7Bnome-do-repositorio%7D/static/css/main.6574b300.chunk.css"
+  },
+  {
+    "revision": "86898c4d62787336f862",
+    "url": "%7Busername%7D.github.io/%7Bnome-do-repositorio%7D/static/js/main.86898c4d.chunk.js"
+  },
+  {
+    "revision": "1873a7e44e2a25ad55d0",
+    "url": "%7Busername%7D.github.io/%7Bnome-do-repositorio%7D/static/css/1.ba64fd5c.chunk.css"
+  },
+  {
+    "revision": "1873a7e44e2a25ad55d0",
+    "url": "%7Busername%7D.github.io/%7Bnome-do-repositorio%7D/static/js/1.1873a7e4.chunk.js"
+  },
+  {
+    "revision": "ad9f6ac73256648872db",
+    "url": "%7Busername%7D.github.io/%7Bnome-do-repositorio%7D/static/js/runtime~main.ad9f6ac7.js"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "%7Busername%7D.github.io/%7Bnome-do-repositorio%7D/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "%7Busername%7D.github.io/%7Bnome-do-repositorio%7D/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "%7Busername%7D.github.io/%7Bnome-do-repositorio%7D/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "%7Busername%7D.github.io/%7Bnome-do-repositorio%7D/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "%7Busername%7D.github.io/%7Bnome-do-repositorio%7D/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "%7Busername%7D.github.io/%7Bnome-do-repositorio%7D/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "%7Busername%7D.github.io/%7Bnome-do-repositorio%7D/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "%7Busername%7D.github.io/%7Bnome-do-repositorio%7D/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "7f3f4c9ff8a26475f4b93f817f2c799c",
+    "url": "%7Busername%7D.github.io/%7Bnome-do-repositorio%7D/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "%7Busername%7D.github.io/%7Bnome-do-repositorio%7D/precache-manifest.2ec12204d6224134c691d42182c49f0e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("%7Busername%7D.github.io/%7Bnome-do-repositorio%7D/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
